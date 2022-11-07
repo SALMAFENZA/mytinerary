ba1da24c28cc0a86c1e3abb76bc35eb6ac1d716d
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-07 17:58:48 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Home from "../src/Pages/Home";
 import NotFound from "./Pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import SignIn from "./Pages/SignIn";
 
 function App() {
   return (
@@ -9,9 +11,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/SignIn" element={<SignIn />} />
           <Route path="*" element={<NotFound />} />
+         
         </Routes>
       </BrowserRouter>
+      <Footer/>
     </>
   );
 }
