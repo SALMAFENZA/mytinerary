1107f9a10a8d9d5d71549c4569a9abd4e41271ec
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-07 16:32:42 -0300

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DataCities from '../Data/DataCities'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+  return (
+
+<>
+    <DataCities/>
+    <Footer/>
+
+</>
+
+  )
+}
