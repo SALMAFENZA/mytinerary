c205f9027c5ca486436f0058e1c6c4a488e4db4f
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-07 19:00:10 -0300

    Create NewHotel.jsx

---
+++ src/Pages/NewHotel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewCityCreate from '../Components/Cities/NewHotel'
+
+export default function NewHotel() {
+  return (
+    <>
+    <NewCityCreate/>
+    </>
+  )
+}
