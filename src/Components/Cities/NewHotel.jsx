75fccd418be48a28f1c4ce94a546de47e0315e81
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-07 18:59:54 -0300

    Create NewHotel.jsx

---
+++ src/Components/Cities/NewHotel.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useRef } from 'react'
+
+export default function NewCity() {
+
+const nameRef = useRef()
+const CapacityRef = useRef()
+const citiIdRef = useRef()
+const photoRef = useRef()
+const citiuserIdRef = useRef()
+
+function create (e){
+  e.preventDefault();  /// Evita que la página se recargue al llenar el formulario
+  
+  
+  const dataHotel = {
+    name: nameRef.current.value,
+    capacity: CapacityRef.current.value,
+    citiId: citiIdRef.current.value,
+    photo: photoRef.current.value,
+    userId: citiuserIdRef.current.value,
+  };
+console.log(dataHotel)
+
+localStorage.setItem("hotel", JSON.stringify(dataHotel) )
+
+
+}
+
+
+
+
+
+
+  return (
+
+    <form className="formCity" onSubmit={create}>
+    <div className="form-Citybody">
+      <h1 className='Citytitle'>Hotel</h1>
+
+
+      <input type="text" placeholder="Name" className='form__input' ref={nameRef}/>
+      <input type="text" placeholder="Capacity" className='form__input' ref={CapacityRef}/>
+      <input type="text" placeholder="City" className='form__input' ref={citiIdRef}/>
+      <input type="text" placeholder="Photo" className='form__input' ref={photoRef}/>
+      <input type="text" placeholder="User" className='form__input' ref={citiuserIdRef}/>
+      
+
+
+
+      <div className="submit">
+        <button className='submit2'>Create</button>
+      </div>
+    </div>
+  </form>
+  )
+}
