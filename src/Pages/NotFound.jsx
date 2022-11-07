5f22379dd3c47223dae8663a28a5cebc228475ea
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-07 17:19:01 -0300

    Create NotFound.jsx

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Error404 from "../Components/Error404";
+import Footer from "../Components/Footer";
+
+export default function NotFound() {
+  return (
+    <>
+      <Error404 />
+      <Footer />
+    </>
+  );
+}
