7016c11ba9dde3961c91271a353c80103b7ed532
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-18 14:01:54 -0300

    Update City.jsx

--- src/Pages/City.jsx
+++ src/Pages/City.jsx
@@ -1,118 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import  cities  from '../Data/dataCities'
-import '../Styles/City.css'
-import {touristActivity} from "../Data/touristActivity"
-import axios from "axios"
+import cities from "../Data/dataCities";
+import "../Styles/City.css";
+import { touristActivity } from "../Data/touristActivity";
+import axios from "axios";
 
+const DetailsCities = () => {
+  const { id } = useParams();
+  const [city, setCity] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/cities/${id}`)
 
-const DetailsCities = () =>  {
+      .then((res) => setCity(res.data.response))
+      .then((res) => console.log(res))
 
+      .catch((err) => console.log(err));
+  }, [id]);
 
+  console.log(city);
 
-    axios({
-        method: 'READ',
-        url: `http://localhost:8000/api/cities${idcity}`,     
+  console.log(id);
 
-      })
-      .then(res => DetailsCities(res.data.city))
-      .catch(err => console.log(err))
+  return (
+    <>
+      <div className="cont-details-city">
+        <div className="card-details-city">
+          <div className="title-details-city">
+            <h5 className="title-city">{city.name}</h5>
+          </div>
 
-      console.log(idcity)       
+          <div className="card-img-city">
+            <img className="img-city" src={city.photo} alt={""} />
+            <h4 className="tittle-details-city">Population: {city.population}</h4>
+          </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    let {idcity} = useParams()
-
-
-    let detailSelect = cities.find( datacities=> datacities.id ===  idcity)
-
-    let activity = touristActivity.filter(activity => activity.citild === idcity)
-    console.log(activity)
-
-    return (
-        <>
-
-        <div className="cont-details-city">
-
-        <div className="card-details-city"  >
-
-            <div className="title-details-city">
-
-                <h5 className="title-city">{detailSelect.name}</h5>
-
+          <div />
+          {
+            <div className="card-details-tourist">
+              {[].map((e) => {
+                return <div>{e.description}</div>;
+              })}
             </div>
-
-
-            <div className="card-img-city"> 
-
-                <img className="img-city" src= {detailSelect.photo} alt={detailSelect} /> 
-
-            </div>
-
-        <div/>
-{ 
-        <div className="card-details-tourist" > 
-            { activity.map((e) => {
-                return(
-                    <div>{e.description}</div>
-                )
-            } ) }
+          }
         </div>
-        }
-
-    </div>
 
         <div>
-            {activity.map((e)=>{
-                return(
-
-                <>
-            <div className="cont-city-turist">
-
-                    <div className="cont-details-tourist">
-
-                        <div className="title-city"> {e.name} </div>
-
-                        <div className="cont-img-details">
-
-                            <img className="img-details-city"  src={e.photo[0]} alt={e.name}/>
-
-                        </div>
-
-                        <div className='cont-desciption-city'>
-
-                            <div className="cont-text-city"> {e.description} </div>
-
-                            <div className="cont-text-city"> U$D : {e.price}</div>
-
-                            <div className="cont-text-city"> time duration :{e.duration}</div>
-
-                        </div>
-
+          {[].map((e) => {
+            console.log(e)
+            return (
+              <>
+                <div className="cont-city-turist">
+                  <div className="cont-details-tourist">
+                    <div className="title-city"> {e.name} </div>
+                    <div className="cont-img-details">
+                      <img
+                        className="img-details-city"
+                        src={e.photo[0]}
+                        alt={e.name}
+                      />
                     </div>
+                    <div className="cont-desciption-city">
+                      <div className="cont-text-city"> {e.description} </div>
 
+                      <div className="cont-text-city"> U$D : {e.price}</div>
+
+                      <div className="cont-text-city">
+                        {" "}
+                        time duration :{e.duration}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                </>
-
-                )
-            })}
+              </>
+            );
+          })}
         </div>
-
-        </div>
-        </>
-
-)}
-export default DetailsCities
+      </div>
+    </>
+  );
+};
+export default DetailsCities;
