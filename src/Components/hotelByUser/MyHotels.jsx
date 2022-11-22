4f8454cec43f9ee232b4b3ca390d66aa40977495
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-22 09:09:54 -0300

    fix-mt-02-40

--- src/Components/hotelByUser/MyHotels.jsx
+++ src/Components/hotelByUser/MyHotels.jsx
@@ -1,66 +1,124 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import axios from "axios";
 import { BASE_URL } from "../../api/url";
-import Carduser from "./CardUser";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { useRef } from 'react';
 import '../../Styles/myHotels.css'
 
-export default function MyHotels() {
-  let [hotel, setHotel] = useState([]);
-  let navigate = useNavigate();
-  useEffect(() => {
-    console.log(hotel);
-    axios
-      .get(`${BASE_URL}/api/myhotels?userId=636f1edc14f79b76f5e442ba`)
-      .then((res) => setHotel(res.data.response))
-      .catch((err) => err.message);
-  }, []);
+export default function NewHotels() {
+  const nameRef = useRef()
+  const capacityRef = useRef()
+  const photoRef = useRef()
+  const userIdRef = useRef()
+  const citiIdRef = useRef()
 
-  let Delete = (e) => {
-    let id = e.target.value;
-    Swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        Swal("!has been deleted!", {
-          icon: "success",
-        });
-        axios
-          .delete(`${BASE_URL}/api/hotels/${id}`)
-          .then((res) => setHotel(res.data.response))
-          .catch((err) => err.message);
-      } else {
-        Swal("Your hotel is safe!");
+  async function submit(e){
+      e.preventDefault();
+      const dataHotel ={
+          name: nameRef.current.value,
+          capacity: capacityRef.current.value,
+          photo: photoRef.current.value,
+          userId: userIdRef.current.value,
+          citiId: citiIdRef.current.value
       }
-      navigate("/myhotels");
-    });
-  };
+      try{
+          let res = await axios.post(`${BASE_URL}/api/hotels`, dataHotel)
+          let res2 = await axios.get(`${BASE_URL}/api/hotels`)
+          let hotelCreated = res2.data.response
+          if(res.data.success){
+              let timerInterval
+              Swal.fire({
+                title: 'Hotel was Created succesfully!',
+                html: 'Redirecting to that page in <b></b> miliseconds.',
+                timer: 1500,
+                timerProgressBar: true,
+                didOpen: () => {
+                  Swal.showLoading()
+                  const b = Swal.getHtmlContainer().querySelector('b')
+                  timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                  }, 100)
+                },
+                willClose: () => {
+                  clearInterval(timerInterval)
+                  hotelCreated.filter(e => e.name === dataHotel.name).map(e => window.location.href = `/detailshotels/${e._id}`)
+                }
+              }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                  console.log('I was closed by the timer')
+                }
+              })
+          }
+          else{
+              Swal.fire({
+                  icon: 'error',
+                  title: 'We found an error...',
+                  text: `Errors: ${res.data.message.join(', ')}`,
+                })
+          }
+      }catch(err){
+          if(err.response.data.message === `hotels validation failed: userId: Cast to ObjectId failed for value "${dataHotel.userId}" (type string) at path "userId" because of "BSONTypeError"`){
+              Swal.fire({
+                  icon: 'error',
+                  title: 'Error 404',
+                  text: 'User ID is invalid. Please try again!',
+                })
+          }else if(err.response.data.message === `hotels validation failed: citiId: Cast to ObjectId failed for value "${dataHotel.citiId}" (type string) at path "citiId" because of "BSONTypeError"`){
+              Swal.fire({
+                  icon: 'error',
+                  title: 'Error 404',
+                  text: 'City ID is invalid. Please try again!',
+                })
+          }else{
+              Swal.fire({
+                  icon: 'error',
+                  title: 'Error 404',
+                  text: 'User ID and City ID are invalid. Please try again!',
+                })
+          }
+      }
+  }
   return (
-    <div>
-      <div className="text-title-myhotels">
-        <h1>My hotels</h1>
-      </div>
-      <div>
-        {hotel.length > 0 ? (
-          hotel.map((item) => (
-            <Carduser
-              name={item.name}
-              erase={Delete}
-              photo={item.photo}
-              key={item._id}
-              id={item._id}
-              description={item.capacity}
-            />
-          ))
-        ) : (
-          <h2>Hotels not found</h2>
-        )}
-      </div>
-    </div>
+
+          <form className="form-hotel2" onSubmit={submit}>
+              <div className="form-body2">
+                  <h1 className='title'>New Hotel</h1>
+                  <h2 className='title2'>¡Create Hotel Card!</h2>
+                  <input
+                      type="text"
+                      placeholder="Hotel Name"
+                      className='form__input'
+                      ref={nameRef}
+                  />
+                  <input
+                      type="text"
+                      placeholder="Photo (URL)"
+                      className='form__input'
+                      ref={photoRef}
+                  />
+                  <input
+                      type="text"
+                      placeholder="Capacity"
+                      className='form__input'
+                      ref={capacityRef}
+                  />
+                  <input
+                      type="text"
+                      placeholder="City ID"
+                      className='form__input'
+                      ref={citiIdRef}
+                      />
+                  <input
+                      type="text"
+                      placeholder="Your User ID"
+                      className='form__input'
+                      ref={userIdRef}
+                  />
+                  <div className="submit">
+                      <button className='submit2'>Create</button>
+                  </div>
+              </div>
+          </form>
   );
-}
+};
