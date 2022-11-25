a1d7f193811888b9f67c14578e1a49818d85fae8
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-24 21:36:04 -0300

    Update Cities.jsx

--- src/Pages/Cities.jsx
+++ src/Pages/Cities.jsx
@@ -1,93 +1,80 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link as NavLink } from "react-router-dom";
 import { ButtonNav } from "../Components/ButtonNav";
-import { useDispatch, useSelector } from "react-redux";
-import citiesAction from "../redux/actions/citiesAction";
 import "../Styles/CityCard.css";
-import axios from "axios";
-
+import { useGetAllQuery, useGetAllContinentQuery } from "../redux/citiesAPI";
 
 export default function CardCities() {
-    const [checkboxArray, setCheckboxArray] = useState([]);
-    const [checks, setChecks] = useState([]);
-    const checkRef = useRef();
-    const searchRef = useRef();
+  const [search, setSearch] = useState("");
+  const checkRef = useRef();
+  const searchRef = useRef();
+  const [checkbox, setChecbox] = useState([]);
 
-    let { citiesList, searchValue, checked} = useSelector((store) => store.citiesReducer);
-    console.log(citiesList);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        axios
-            .get(`http://localhost:8000/api/cities`)
-            .then((res) => setCheckboxArray(res.data.city))
-            .catch((err) => console.log(err));
-    }, []);
-    useEffect(() => {
-        if (!citiesList){
-            dispatch(citiesAction.getCities({
-            searchValue: "",
-            checked: ""
-           }))}
-    },[]);
+  let { data } = useGetAllQuery({ search, checkbox });  
+  let { data: checkboxs } = useGetAllContinentQuery();
 
-    const checkBox = Array.from(new Set(checkboxArray?.map((e) => e.continent)));
+  let cities = data?.city;
+  let checkboxArray = checkboxs?.city;
 
-function filterCities(){
-let value= searchRef.current.value;
-dispatch(citiesAction.getCities({ searchValue:value,checked }));
-}
+  const checkBox = Array.from(new Set(checkboxArray?.map((e) => e.continent)));
 
-    function filterCheck(e) {
-let checks =(Array.from(checkRef.current).filter(check => check.checked  === true).map(check=>check.id))
-    
-        dispatch(citiesAction.getCities({ searchValue,checked:checks }));
-    }
-    return (
-        <>
-            <div className="filters">
-                <NavLink to="/newcity">
-                    <li className="addCity">
-                        <ButtonNav className="ancorLink" n2="New City">
-                            !Add your city!
-                        </ButtonNav>
-                    </li>
-                </NavLink>
+  function filterCities() {
+    setSearch(searchRef.current.value);
+  }
+
+  function filterCheck(e) {
+    let checks = Array.from(checkRef.current)
+      .filter((check) => check.checked === true)
+      .map((check) => check.id);
+    setChecbox(checks);
+    console.log(checks);
+  }
+  return (
+    <>
+      <div className="filters">
+        <NavLink to="/newcity">
+          <li className="addCity">
+            <ButtonNav className="ancorLink" n2="New City">
+              !Add your city!
+            </ButtonNav>
+          </li>
+        </NavLink>
+      </div>
+      <div className="filters">
+        <form ref={checkRef}>
+          {checkBox?.map((e) => {
+            return (
+              <label>
+                {e}
+                <input id={e} type="checkbox" onClick={filterCheck} />
+              </label>
+            );
+          })}
+        </form>
+        <label className="inputs">Serch Here</label>
+        <input
+          onKeyUp={filterCities}
+          ref={searchRef}
+          className="search"
+          type="text"
+        />
+      </div>
+
+      {cities?.map((e) => {
+        return (
+          <div>
+            <div className="box2">
+              <div className="cont-img">
+                <img className="image" src={e.photo} alt={e.name} />
+              </div>
+              <h3>{e.name}</h3>
+              <NavLink to={`/city/${e._id}`} className="nav-cities">
+                See More
+              </NavLink>
             </div>
-            <div className="filters">
-                <form ref={checkRef}> 
-                {checkBox?.map((e) => {
-                    return (
-                        <label>
-                            {e}
-                            <input id={e} type="checkbox" onClick={filterCheck} />
-                        </label>
-                    );
-                })}
-                </form>
-                <label className="inputs">Serch Here</label>
-                <input onKeyUp={filterCities}
-                    ref={searchRef}
-                    className="search"
-                    type="text"
-                />
-            </div>
-
-            {citiesList?.map((e) => {
-                return (
-                    <div>
-                        <div className="box2">
-                            <div className="cont-img">
-                                <img className="image" src={e.photo} alt="hotel" />
-                            </div>
-                            <h3>{e.name}</h3>
-                            <NavLink to={`/city/${e._id}`} className="nav-cities">
-                                See More
-                            </NavLink>
-                            
-                        </div>
-                    </div>
-                );
-            })}
-        </>
-    );
+          </div>
+        );
+      })}
+    </>
+  );
 }
