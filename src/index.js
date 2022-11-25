9d5a9022576586506c4018da0875c4b4a2cd0c71
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-24 21:35:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import {configureStore} from '@reduxjs/toolkit';
-import rootReducer from './redux/reducers/rootReducer';
-
-// REDUX
-// import rootReducers from "./redux/reducer/rootReducer"
-// import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
-
-const store = configureStore({reducer:rootReducer})
-
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById(`salma`));
 root.render(
