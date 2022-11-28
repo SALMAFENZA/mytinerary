1db082c589820b53e27c51358b517101e3e22745
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-28 05:24:55 -0300

    Update ButtonNav.jsx

--- src/Components/ButtonNav.jsx
+++ src/Components/ButtonNav.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useState } from "react";
 import "../Styles/ButtonNav.css";
 import { Link as NavLink } from "react-router-dom";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function ButtonNav(props) {
   let { n1, n2 } = props;
@@ -11,6 +16,31 @@ function ButtonNav(props) {
   let hide = () => {
     setMostrarOcultar(!mostrarOcultar);
   };
+function logout(){
+  confirmAlert({
+    title: "Log out",
+    message: "Really leave us? 😭",
+    buttons: [
+      {
+        label: "Yes, bye",
+        onClick: async () => {
+localStorage.clear()
+         toast("good bye dear user")
+        },
+      },
+      {
+        label: "No, I'll stay ♥",
+        onClick: () => console.log("Click No"),
+      },
+    ],
+  });
+
+}
+
+
+
+
+
 
   return (
     <div>
@@ -36,6 +66,11 @@ function ButtonNav(props) {
                             <a className="ancorLink"  href="-">{n2}</a>
                         </li>
                     </NavLink>
+                    <NavLink >
+                        <li className="b">
+                            <a className="ancorLink"  href="" onClick={() => logout()} >Log out</a>
+                        </li>
+                    </NavLink>
           </ul>
         </>
       ) : (
@@ -48,6 +83,7 @@ function ButtonNav(props) {
           alt="-"
         />
       )}
+       <ToastContainer />
     </div>
   );
 }
