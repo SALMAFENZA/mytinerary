82ca676255545e9867badf5641fa052dd0a1c285
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-29 15:13:55 -0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,23 +8,11 @@ function Header() {
         return(
         <>
         <header className="main-nav-header">
-                <div className="buton-nav-header">
-                <img className="buton-nav-header" src="https://th.bing.com/th/id/OIP.BcotGjhQr6xA_0kiwr0I3AHaHa?pid=ImgDet&rs=1" width="17%" alt="" />
                     <ButtonNav1 n5="Home" n6="Cities"/> 
-                </div>
-                <div className="buton-nav-header">
-                <img className="buton-nav-header" src="https://th.bing.com/th/id/OIP.JEYHfXEn6eiFCUGpn8uZ-wHaHa?w=219&h=219&c=7&r=0&o=5&pid=1.7" width="30%" alt="" />
-                    <ButtonNav2 n7="Edit a city" n8="Crete a city"/>
-                    
-                
-                </div>
-                <div className="buton-nav-header">
-                    <img className="buton-nav-header" src="https://u7.uidownload.com/vector/461/227/vector-administration-vector-icon-eps.jpg" width="17%" alt="" />
+                    <ButtonNav2 n7="Edit a city" n8="Crete a city"/>                     
                     <ButtonNav n1="Log In" n2="Sign Up"/>
-
-                </div>
         </header>
         </>
-        )
+                )
 }
 export { Header }
