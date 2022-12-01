41c4aafc512a7b628c5e3aeb551dc32a01b6a209
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-12-01 14:18:09 -0300

    Update NewCity.jsx

--- src/Components/Cities/NewCity.jsx
+++ src/Components/Cities/NewCity.jsx
@@ -1,3 +1,71 @@
+// import React from "react";
+// import { useRef } from "react";
+
+// export default function NewCity() {
+//   const cityRef = useRef();
+//   const countryRef = useRef();
+//   const populationRef = useRef();
+//   const photoRef = useRef();
+//   const userRef= useRef();
+  
+//   function create(e) {
+//     e.preventDefault(); /// Evita que la página se recargue al llenar el formulario
+
+//     const dataCity = {
+//       name: cityRef.current.value,
+//       continent: countryRef.current.value,
+//       photo: photoRef.current.value,
+//       population: populationRef.current.value,
+//       userRef: userRef.current.value,
+
+//     };
+    
+//   }
+//   console.log(dataCity);
+
+//   return (
+//     <form className="formCity" onSubmit={create}>
+//       <div className="form-Citybody">
+//         <h1 className="Citytitle">City</h1>
+
+//         <input
+//           type="text"
+//           placeholder="City"
+//           className="put"
+//           ref={cityRef}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Country"
+//           className="put"
+//           ref={countryRef}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Population"
+//           className="form__input"
+//           ref={populationRef}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Photo"
+//           className="form__input"
+//           ref={photoRef}
+//         />
+//         <input
+//           type="text"
+//           placeholder="User"
+//           className="form__input"
+//           ref={userRef}
+//         />
+//         <div className="submit">
+//           <button className="submit2">Create</button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// }
+
 import React from "react";
 import { useRef } from "react";
 
