a663a073c159eedebeb6e3a1597b7a86431bb434
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-30 21:44:16 -0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import citiesAPI from "./reducers/citiesAPI";
 import itinerariesAPI from "./reducers/itinerariesaAPI";
 import userAPI from "./reducers/userAPI";
+import reactionsAPI from "./reducers/reactions";
 
 export default configureStore({
 reducer: {
 [citiesAPI.reducerPath] : citiesAPI.reducer,
 [itinerariesAPI.reducerPath] : itinerariesAPI.reducer,
-[userAPI.reducerPath] : userAPI.reducer
+[userAPI.reducerPath] : userAPI.reducer,
+[reactionsAPI.reducerPath] : reactionsAPI.reducer
 }, 
 middleware: (getDefaultMiddleware) =>
 getDefaultMiddleware()
 .concat(citiesAPI.middleware)
 .concat(itinerariesAPI.middleware)
 .concat(userAPI.middleware)
+.concat(reactionsAPI.middleware)
 })
