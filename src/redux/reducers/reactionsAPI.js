61783492a6b9e8b83a20fa9d8b3d34b5ed8110ed
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-12-01 22:20:27 -0300

    Create reactionsAPI.js

---
+++ src/redux/reducers/reactionsAPI.js
@@ -0,0 +1,45 @@
+import { BASE_URL } from "../../api/url";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+/// Redux API ? Una herramienta de redux toolkit que permite hacer peticiones ♥
+const reactionsAPI = createApi({
+  reducerPath: "reactionsAPI",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL, /// LocalHost:8000
+  }),
+
+  endpoints: (builder) => ({
+    //// endpoints son las acciones que realizará el redux. ♥
+    addReaction: builder.mutation({
+      query: ({id , token }) => ({
+        url: `/api/reactions/${id}`,
+        method: "PUT",
+        headers: {
+          Authorization: "Bearer " + `${token}`,
+        },
+      }),
+    }),
+    deleteReaction: builder.mutation({
+      query: ({id , token }) => ({
+        url: `/api/reactions/`,
+        method: "DELETE",
+        body: id,
+        headers: {
+          Authorization: "Bearer " + `${token}`, 
+        },
+      }),
+    }),
+    getReaction: builder.mutation({
+      query: ({ itineraryId, userId }) => ({
+        url: `/api/reactions?itineraryId=${itineraryId}&userId=${userId}`,
+      }),
+    }),
+  }),
+});
+export default reactionsAPI;
+export const {
+  useDeleteReactionMutation,
+  useAddReactionMutation,
+  useGetReactionMutation,
+} = reactionsAPI;
